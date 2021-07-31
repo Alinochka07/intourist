66468b3f6ba9341e0c9b1f3633a6d4fecbe8a5f4
Alina Akylbekova <Alinochka07@Kanatbeks-iMac.local>
2021-07-31 17:40:31 +0900

    added unit tests

--- places/tests.py
+++ places/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse 
+from django.db.models import QuerySet
+from places.models import Place
+from .factories import PlaceFactory
 
-# Create your tests here.
+
+class PlacesListTestCase(TestCase):
+    def test_open_list_success(self):
+        place_1 = PlaceFactory(name='Cool place', description='Visit any time')
+        place_2 = PlaceFactory()
+
+        url = reverse('places-list') #/places
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        places = response.context.get('places')
+        self.assertIsInstance(places, QuerySet)
+        
+        # place_2_db = Place.objects.get(name='Place number 1')
+        # self.assertEqual(place_2_db.location, places[1].location)
+        self.assertEqual('Location - 1', places[1].location)
+        self.assertEqual(places[0].description, 'Visit any time')
+
+class PlaceCreateTestCase(TestCase):
+    def test_create_place_success(self):
+        url = reverse('create-place')
+        data = {
+            'name': 'Issyk-Kul',
+            'location': 'Karakol region',
+            'description': 'Lake in Kyrgyzstan' 
+        }
+        response = self.client.post(url, data)
+        place = Place.objects.last()
+        self.assertEqual(place.name, 'Issyk-Kul')
+        self.assertEqual(place.location, 'Karakol region')
+        self.assertEqual(place.description, 'Lake in Kyrgyzstan')
+
